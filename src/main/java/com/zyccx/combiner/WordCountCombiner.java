e2b611794d7314102200dfa7689d2d7d5ac8a4ad
ZhangYichao <yichao0803@qq.com>
2021-03-09 21:55:35 +0800

    增加combiner 测试类

---
+++ src/main/java/com/zyccx/combiner/WordCountCombiner.java
@@ -0,0 +1,21 @@
+package com.zyccx.combiner;
+
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Reducer;
+
+import java.io.IOException;
+
+public class WordCountCombiner extends Reducer<Text, IntWritable, Text, IntWritable> {
+    IntWritable outValue = new IntWritable();
+
+    @Override
+    protected void reduce(Text key, Iterable<IntWritable> values, Context context) throws IOException, InterruptedException {
+        Integer sum = 0;
+        for (IntWritable value : values) {
+            sum += value.get();
+        }
+        outValue.set(sum);
+        context.write(key, outValue);
+    }
+}
---
+++ src/main/java/com/zyccx/combiner/WordCountDriver.java
@@ -0,0 +1,55 @@
+package com.zyccx.combiner;
+
+import com.zyccx.wc.v1.WordCountMapper;
+import com.zyccx.wc.v1.WordCountReducer;
+import org.apache.hadoop.conf.Configured;
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Job;
+import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
+import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
+import org.apache.hadoop.util.Tool;
+import org.apache.hadoop.util.ToolRunner;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class WordCountDriver extends Configured implements Tool {
+    Logger logger = LoggerFactory.getLogger(WordCountDriver.class);
+
+    @Override
+    public int run(String[] args) throws Exception {
+
+        // 1、声明 Job和配置Job
+        final Job job = Job.getInstance(getConf(), "Word Count Tool");
+
+        // 2、设置 jar
+        job.setJarByClass(getClass());
+
+        // 3、设置 mapper 和 reducer
+        job.setMapperClass(WordCountMapper.class);
+        job.setReducerClass(WordCountReducer.class);
+
+        // 4、设置 mapper 的输出 kv 类型
+        job.setMapOutputKeyClass(Text.class);
+        job.setMapOutputValueClass(IntWritable.class);
+        //job.setNumReduceTasks(0);
+
+        job.setCombinerClass(WordCountCombiner.class);
+        // 5、设置最终的输出 kv 类型
+        job.setOutputKeyClass(Text.class);
+        job.setOutputValueClass(IntWritable.class);
+
+        // 6、设置输入和输出的路径
+        FileInputFormat.setInputPaths(job, new Path("E:\\hadoop\\wordCount\\input"));
+        FileOutputFormat.setOutputPath(job, new Path("E:\\hadoop\\output-combiner-3"));
+
+        // 7、提交任务
+        return job.waitForCompletion(true) ? 0 : 1;
+    }
+
+    public static void main(String[] args) throws Exception {
+        int run = ToolRunner.run(new WordCountDriver(), args);
+        System.exit(run);
+    }
+}
